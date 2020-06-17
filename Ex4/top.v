2cf82efa0bcc26a07fe9cdb9717dcd780af64eb7
Bambiekkki <46208339+Bambiekkki@users.noreply.github.com>
2020-06-17 14:54:37 +0800

    solution to EX4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,19 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+module dice(clk,rst,button,throw);
+
+	input clk, rst, button;
+	output reg [2:0] throw;
+
+	always @(posedge clk) 
+	begin
+	if (rst || throw == 3'b111)
+		throw <= 3'b001;
+	else if (button && throw != 3'b110)
+		throw <= throw + 1;
+	else if (button && throw == 3'b110)
+		throw <= 3'b001;
+	end
+
+endmodule 
