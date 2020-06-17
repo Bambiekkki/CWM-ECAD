57c9e2e4a8638d232a075b34dfd2d2c1e5e2c0e1
Bambiekkki <46208339+Bambiekkki@users.noreply.github.com>
2020-06-17 17:50:15 +0800

    solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -20,3 +20,18 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+module top(
+        input clk,sel,rst,button,
+        output [2:0]result
+);
+  
+wire [2:0]throw;
+wire [2:0]state;
+  
+dice my_dice(.clk(clk), .rst(rst), .button(button), .throw(throw));
+traffic_lights trafic_lights(.clk(clk), .red(state[0]), .amber(state[1]), .green(state[2]));
+multiplexer my_multiplexer( .a(throw), .b(state), .sel(sel), .out(result));
+
+
+endmodule
+
