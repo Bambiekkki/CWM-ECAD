a5878488ad1a1ff37722f7f61e2c7cbf9361b267
Bambiekkki <46208339+Bambiekkki@users.noreply.github.com>
2020-06-17 15:10:50 +0800

    solution to Ex5

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,24 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+wire red, amber, green;
+
+initial begin
+clk = 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+$display("red=%h",red);
+$display("amber=%h",amber);
+$display("green=%h",green);
+end
+
+traffic_lights mytraffic(clk,red,amber,green);
+
+endmodule
