bf9160e4f468028729230de4394b938ff699dc8e
Bambiekkki <46208339+Bambiekkki@users.noreply.github.com>
2020-06-17 14:40:06 +0800

    solution to Ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -14,15 +14,37 @@ module top_tb(
     );
     
 //Todo: Parameters
-
+    parameter CLK_PERIOD = 10;
+    
 //Todo: Regitsers and wires
-
+    reg clk,rst,enable,direction;
+    wire [7:0] counter_out;
+    
 //Todo: Clock generation
-
+    initial begin
+    clk= 1'b0;
+    forever 
+    #(CLK_PERIOD/2) clk=~clk;
+    end
+    
 //Todo: User logic
+    initial begin
+    rst=1;
+    enable=0;
+    direction=1;
+    #10
+    enable=1;
+    direction=1;
+    #20
+    direction=0;
+    #10
+    direction=1;
+    $display("counter_out=%h",counter_out);
+    end
     
 //Todo: Finish test, check for success
 
 //Todo: Instantiate counter module
- 
+    counter my_counter(clk,rst,enable,direction,counter_out);
+    
 endmodule 
