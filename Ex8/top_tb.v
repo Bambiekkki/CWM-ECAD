2141b407fb7a5d3fddda1b1105b67e973c040cd6
Bambiekkki <46208339+Bambiekkki@users.noreply.github.com>
2020-06-18 20:17:38 +0800

    Update top_tb.v

--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -7,3 +7,40 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg rst;
+reg [2:0]a;
+reg [2:0]b;
+reg read;
+wire [5:0] result;
+
+initial begin
+clk = 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+end
+
+initial begin
+a = 0;
+b = 0;
+rst=1; 
+read=1;
+forever begin
+	#CLK_PERIOD
+	$display("Result=%h",result);
+	a = a+1; //cycle through a=0->7 x b=0->7
+	if (a==0)
+		b = b+1;
+	end
+end
+
+times my_times(.clk(clk), .a(a), .b(b), .rst(rst), .read(read), .result(result)); //top instance
+
+
+endmodule
